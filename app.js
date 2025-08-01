7f922c71bccafe02a5f9d9543e74fd916bac63c4
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-01 15:20:26 +0000

    Implemented getStates API

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require("express");
-const { open } = sqlite;
+const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
 const app = express();
 
 const dbPath = path.join(__dirname, "covid19India.db");
+let db = null;
+
+const InitializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server listening at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+InitializeDBAndServer();
+
+app.get("/states", async (request, response) => {
+  const getStatesQuery = `select * from state order by state_id;`;
+  const states = await db.all(getStatesQuery);
+  response.send(states);
+});
